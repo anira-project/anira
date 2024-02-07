6888ccb9428f158e40d6efa9913dc40360510474
Fares Schulz <schulzfares@gmail.com>
2024-02-07 21:56:42 +0100

    New ProcessBlockFixture.cpp, cleaned defineBenchmark

--- include/anira/benchmark/ProcessBlockFixture.h
+++ include/anira/benchmark/ProcessBlockFixture.h
@@ -11,81 +11,28 @@ namespace benchmark {
 class ProcessBlockFixture : public ::benchmark::Fixture {
 public:
 
-    ProcessBlockFixture() {
-        bufferSize = std::make_unique<int>(0);
-        repetition = std::make_unique<int>(0);
-    }
-    ~ProcessBlockFixture() {
-        bufferSize.reset(); // buffersize and repetetion don't need to be reset when the plugin is reset
-        repetition.reset();
-    }
+    ProcessBlockFixture();
+    ~ProcessBlockFixture();
 
-    void constructInferenceHandler(PrePostProcessor& prePostProcessor, InferenceConfig& config) {
-        inferenceHandler = std::make_unique<InferenceHandler>(prePostProcessor, config);
-    }
+    void initializeIteration();
+    bool bufferHasBeenProcessed();
+    void pushRandomSamplesInBuffer(anira::HostAudioConfig hostAudioConfig);
+    int getBufferSize();
+    int getRepetition();
+    void interationStep(std::chrono::_V2::system_clock::time_point start, std::chrono::_V2::system_clock::time_point end, int& iteration, ::benchmark::State& state);
+    void repetitionStep();
 
-    void prepareInferenceHandler(anira::HostAudioConfig hostAudioConfig) {
-        inferenceHandler->prepare(hostAudioConfig);
-    }
-
-    void pushSamplesInBuffer(anira::HostAudioConfig hostAudioConfig) {
-        for (size_t channel = 0; channel < hostAudioConfig.hostChannels; channel++) {
-            for (size_t sample = 0; sample < hostAudioConfig.hostBufferSize; sample++) {
-                buffer->setSample(channel, sample, randomSample());
-            }
-        }
-    }
-
-    void setInferenceBackend(anira::InferenceBackend inferenceBackend) {
-        inferenceHandler->setInferenceBackend(inferenceBackend);
-    }
-
-    bool isInitializing() {
-        return inferenceHandler->getInferenceManager().isInitializing();
-    }
-
-    int getNumReceivedSamples() {
-        return inferenceHandler->getInferenceManager().getNumReceivedSamples();
-    }
-
-    int getBufferSize() {
-        return *bufferSize;
-    }
-
-    float** getArrayOfWritePointers() {
-        return buffer->getArrayOfWritePointers();
-    }
-
-    int getRepetition() {
-        return *repetition;
-    }
-
-    void repetitionStep() {
-        *repetition += 1;
-    }
-
-    inline static std::unique_ptr<anira::InferenceHandler> inferenceHandler = nullptr;
+    inline static std::unique_ptr<anira::InferenceHandler> m_inferenceHandler = nullptr;
+    inline static std::unique_ptr<anira::AudioBuffer<float>> m_buffer = nullptr;
 
 private:
-    inline static std::unique_ptr<int> bufferSize = nullptr;
-    inline static std::unique_ptr<anira::AudioBuffer<float>> buffer = nullptr;
-    inline static std::unique_ptr<int> repetition = nullptr;
+    inline static std::unique_ptr<int> m_bufferSize = nullptr;
+    inline static std::unique_ptr<int> m_repetition = nullptr;
+    bool m_init = false;
+    int m_prev_num_received_samples = 0;
 
-    void SetUp(const ::benchmark::State& state) {
-        buffer = std::make_unique<AudioBuffer<float>>(1, *bufferSize);
-        if (*bufferSize != (int) state.range(0)) {
-            *bufferSize = (int) state.range(0);
-            std::cout << "\n------------------------------------------------------------------------------------------------" << std::endl;
-            std::cout << "Sample Rate 44100 Hz | Buffer Size " << *bufferSize << " = " << (float) * bufferSize * 1000.f/44100.f << " ms" << std::endl;
-            std::cout << "------------------------------------------------------------------------------------------------\n" << std::endl;
-            buffer->initialize(1, (int) *bufferSize);
-            repetition.reset(new int(0));
-        }
-    }
-    void TearDown(const ::benchmark::State& state) {
-        buffer.reset();
-        inferenceHandler.reset();
-    }
+    void SetUp(const ::benchmark::State& state);
+    void TearDown(const ::benchmark::State& state);
 };
 
 } // namespace benchmark
