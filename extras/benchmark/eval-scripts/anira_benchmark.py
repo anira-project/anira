874cae367e3a586a1bf63caae8940b3c2628162f
Fares Schulz <schulzfares@gmail.com>
2024-02-15 16:23:29 +0100

    Implemented Repetition index for benchmark evaluation

--- extras/benchmark/eval-scripts/anira_benchmark.py
+++ extras/benchmark/eval-scripts/anira_benchmark.py
@@ -16,29 +16,37 @@ def get_list_from_log(file_path: str, list: list=None) -> list:
         model = []
         backend = []
         buffer_size = []
+        repetition_index = []
         iteration_count = []
         repetition_count = []
         runtime = []
-        log_list = [operating_system, model, backend, buffer_size, iteration_count, repetition_count, runtime]
+        log_list = [operating_system, model, backend, buffer_size, repetition_index, repetition_count, iteration_count, runtime]
     else:
         log_list = list
 
+    repetition_index = 0
+    old_repeatition_count = 0
     with open(file_path, 'r') as file:
         for line in file:
             if "SingleIteration" in line:
                 match = re.search(r"SingleIteration\/ProcessBlockFixture\/[^\/]*\/([^.]*.[a-zA-Z]*)\/([a-z]*)\/([0-9]*)\/iteration:([0-9]*)\/repetition:([0-9])\s*([0-9]*.[0-9]*)\sms", line)
-                log_list[0].append(os.path.splitext(os.path.basename(file_path))[0])
-                log_list[1].append(match.group(1))
-                log_list[2].append(match.group(2))
-                log_list[3].append(int(match.group(3)))
-                log_list[4].append(int(match.group(5)))
-                log_list[5].append(int(match.group(4)))
-                log_list[6].append(float(match.group(6)))
+                if not "stateful-lstm-libtorch.onnx" == match.group(1): # onnxruntime does not support stateful lstm
+                    if old_repeatition_count != int(match.group(5)):
+                        repetition_index += 1
+                        old_repeatition_count = int(match.group(5))
+                    log_list[0].append(os.path.splitext(os.path.basename(file_path))[0])
+                    log_list[1].append(match.group(1))
+                    log_list[2].append(match.group(2))
+                    log_list[3].append(int(match.group(3)))
+                    log_list[4].append(repetition_index)
+                    log_list[5].append(int(match.group(5)))
+                    log_list[6].append(int(match.group(4)))
+                    log_list[7].append(float(match.group(6)))
     
     return log_list
 
 def get_sequence_statistics_from_list(list: list, measure: str, lenght_slices: int=10) -> list:
-    stat_list = []
+    stat_list = [[]]
     for l in list:
         stat_list.append([])
     for i in range(0, len(list[0]), lenght_slices):
@@ -47,19 +55,20 @@ def get_sequence_statistics_from_list(list: list, measure: str, lenght_slices: i
         stat_list[2].append(list[2][i])
         stat_list[3].append(list[3][i])
         stat_list[4].append(list[4][i])
-        stat_list[5].append(f"{list[5][i]/lenght_slices}")
+        stat_list[5].append(list[5][i])
+        stat_list[6].append(f"{list[6][i]/lenght_slices}")
         if measure == "sequence_mean":
-            stat_list[6].append(np.mean(list[6][i:i+lenght_slices]))
+            stat_list[7].append(np.mean(list[7][i:i+lenght_slices]))
         elif measure == "sequence_median":
-            stat_list[6].append(np.median(list[6][i:i+lenght_slices]))
+            stat_list[7].append(np.median(list[7][i:i+lenght_slices]))
         elif measure == "sequence_max":
-            stat_list[6].append(np.max(list[6][i:i+lenght_slices]))
+            stat_list[7].append(np.max(list[7][i:i+lenght_slices]))
         elif measure == "sequence_min":
-            stat_list[6].append(np.min(list[6][i:i+lenght_slices]))
+            stat_list[7].append(np.min(list[7][i:i+lenght_slices]))
         elif measure == "sequence_iqr":
-            stat_list[6].append(np.abs(np.percentile(list[6][i:i+lenght_slices], 75) - np.percentile(list[6][i:i+lenght_slices], 25)))
+            stat_list[7].append(np.abs(np.percentile(list[7][i:i+lenght_slices], 75) - np.percentile(list[7][i:i+lenght_slices], 25)))
         elif measure == "sequence_std":
-            stat_list[6].append(np.std(list[6][i:i+lenght_slices]))
+            stat_list[7].append(np.std(list[7][i:i+lenght_slices]))
         else:
             raise ValueError("Invalid measure")    
           
@@ -78,8 +87,9 @@ def moving_average(list: list, window: int=3) -> list:
             moving_average_list[2].append(list[2][index])
             moving_average_list[3].append(list[3][index])
             moving_average_list[4].append(list[4][index])
-            moving_average_list[5].append(f"{i} - {i+window-1}")
-            moving_average_list[6].append(np.mean(list[6][index+i:index+i+window]))
+            moving_average_list[5].append(list[5][index])
+            moving_average_list[6].append(f"{i} - {i+window-1}")
+            moving_average_list[7].append(np.mean(list[7][index+i:index+i+window]))
 
     return moving_average_list
 
@@ -97,8 +107,9 @@ def cummulativ_average(list: list) -> list:
                 cummulativ_average_list[2].append(list[2][index])
                 cummulativ_average_list[3].append(list[3][index])
                 cummulativ_average_list[4].append(list[4][index])
-                cummulativ_average_list[5].append(f"0 - {i-1}")
-                cummulativ_average_list[6].append(np.mean(list[6][index:index+i]))
+                cummulativ_average_list[5].append(list[5][index])
+                cummulativ_average_list[6].append(f"0 - {i-1}")
+                cummulativ_average_list[7].append(np.mean(list[7][index:index+i]))
 
     return cummulativ_average_list
 
@@ -106,29 +117,29 @@ def write_list_to_csv(file_path: str, list: list, append: bool=False, top_row_ar
     if not append:
         with open(file_path, 'w', newline='') as file:
             writer = csv.writer(file)
-            top_row = ["Operating System", "Model", "Backend", "Buffer Size", "Repetition Count", "Iteration Count", "Runtime"]
+            top_row = ["Operating System", "Model", "Backend", "Buffer Size", "Repetition Index", "Repetition Count", "Iteration Count", "Runtime"]
             if top_row_argument == "sequence_mean":
-                top_row[5:] = ["Sequence Count", "Mean"]
+                top_row[6:] = ["Sequence Count", "Mean"]
             elif top_row_argument == "sequence_median":
-                top_row[5:] = ["Sequence Count", "Median"]
+                top_row[6:] = ["Sequence Count", "Median"]
             elif top_row_argument == "sequence_max":
-                top_row[5:] = ["Sequence Count", "Max"]
+                top_row[6:] = ["Sequence Count", "Max"]
             elif top_row_argument == "sequence_min":
-                top_row[5:] = ["Sequence Count", "Min"]
+                top_row[6:] = ["Sequence Count", "Min"]
             elif top_row_argument == "sequence_iqr":
-                top_row[5:] = ["Sequence Count", "IQR"]
+                top_row[6:] = ["Sequence Count", "IQR"]
             elif top_row_argument == "sequence_std":
-                top_row[5:] = ["Sequence Count", "STD"]
+                top_row[6:] = ["Sequence Count", "STD"]
             elif top_row_argument == "moving_average":
-                top_row[6] = "Moving Average"
+                top_row[7] = "Moving Average"
             elif top_row_argument == "cummulativ_average":
-                top_row[6] = "Cummulativ Average"
+                top_row[7] = "Cummulativ Average"
             writer.writerow(top_row)
-            writer.writerows(zip(list[0], list[1], list[2], list[3], list[4], list[5], list[6]))
+            writer.writerows(zip(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7]))
     else:
         with open(file_path, 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerows(zip(list[0], list[1], list[2], list[3], list[4], list[5], list[6]))
+            writer.writerows(zip(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7]))
 
 if __name__ == "__main__":
     create_folder("results")
