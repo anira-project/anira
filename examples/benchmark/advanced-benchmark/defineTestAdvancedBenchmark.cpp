cc6fa2c3110bdb8a9e14b49bd4b3c90422b7789f
Fares Schulz <schulzfares@gmail.com>
2024-04-26 18:46:19 +0200

    Benchmarks now also runs main process with realtime priority

--- examples/benchmark/advanced-benchmark/defineTestAdvancedBenchmark.cpp
+++ examples/benchmark/advanced-benchmark/defineTestAdvancedBenchmark.cpp
@@ -1,8 +1,14 @@
 #include <benchmark/benchmark.h>
 #include <gtest/gtest.h>
+#include <anira/anira.h>
 
-TEST(Benchmark, All){
-    // LockMemory();
+TEST(Benchmark, Advanced){
+#if __linux__
+    pthread_t self = pthread_self();
+#elif WIN32
+    HANDLE self = GetCurrentThread();
+#endif
+    anira::system::RealtimeThread::elevateToRealTimePriority(self);
+
     benchmark::RunSpecifiedBenchmarks();
-    std::cout << "--------------------------------------------------------------" << std::endl;
 }
