5cd021222254ca7b63d84c187b058cd2a7ce4516
Fares Schulz <schulzfares@gmail.com>
2024-09-23 22:09:04 +0200

    Removed now unneeded code for downloading libomp dependency

--- cmake/SetupLibTorch.cmake
+++ cmake/SetupLibTorch.cmake
@@ -106,7 +106,6 @@ find_package(Torch REQUIRED)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
 
 # Suppress warnings by setting -w flag as a linker option
-# target_link_directories(torch INTERFACE "/opt/homebrew/opt/libomp/lib")
 target_link_options(torch INTERFACE "-w")
 target_link_options(torch_library INTERFACE "-w")
 
