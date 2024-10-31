fcdac21c136fe24dddf944d0effdf55b7e40bd3b
vackva <valentin.ackva@gmail.com>
2024-10-31 19:17:24 +0100

    updated minimal-onnxruntime example

--- examples/desktop/minimal-inference/onnxruntime/minimal-onnxruntime.cpp
+++ examples/desktop/minimal-inference/onnxruntime/minimal-onnxruntime.cpp
@@ -11,81 +11,108 @@ Licence: MIT
 #include "../../../../extras/desktop/models/stateful-rnn/StatefulRNNConfig.h"
 #include "../../../../extras/desktop/models/hybrid-nn/HybridNNConfig.h"
 #include "../../../../extras/desktop/models/cnn/CNNConfig.h"
+#include "../../../../include/anira/utils/MemoryBlock.h"
+#include "../../../../include/anira/utils/AudioBuffer.h"
 
-void minimal_inference(anira::InferenceConfig config) {
+void minimal_inference(anira::InferenceConfig m_inference_config) {
 
     std::cout << "Minimal OnnxRuntime example:" << std::endl;
     std::cout << "-----------------------------------------" << std::endl;
-    std::cout << "Using model: " << config.m_model_path_onnx << std::endl;
+    std::cout << "Using model: " << m_inference_config.m_model_path_onnx << std::endl;
 
     // Define environment that holds logging state used by all other objects.
-    // Note: One Env must be created before using any other Onnxruntime functionality.
-    Ort::Env env;
+    // Note: One Env must be created before using any other OnnxRuntime functionality.
+    Ort::Env m_env;
     // Define memory info for input and output tensors for CPU usage
-    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+    Ort::MemoryInfo m_memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
     // Define allocator
-    Ort::AllocatorWithDefaultOptions ort_alloc;
+    Ort::AllocatorWithDefaultOptions m_ort_alloc;
 
     // Limit inference to one thread
-    Ort::SessionOptions session_options;
-    session_options.SetIntraOpNumThreads(1);
+    Ort::SessionOptions m_session_options;
+    m_session_options.SetIntraOpNumThreads(1);
 
     // Load the model and create InferenceSession
 #ifdef _WIN32
-    std::wstring modelWideStr = std::wstring(config.m_model_path_onnx.begin(), config.m_model_path_onnx.end());
+    std::wstring modelWideStr = std::wstring(m_inference_config.m_model_path_onnx.begin(), m_inference_config.m_model_path_onnx.end());
     const wchar_t* modelWideCStr = modelWideStr.c_str();
-    Ort::Session session(env, modelWideCStr, session_options);
+    Ort::Session m_session(m_env, modelWideCStr, m_session_options);
 #else
-    Ort::Session session(env, config.m_model_path_onnx.c_str(), Ort::SessionOptions{ nullptr });
+    Ort::Session m_session(m_env, m_inference_config.m_model_path_onnx.c_str(), Ort::SessionOptions{ nullptr });
 #endif
 
-    // Define input data
-    std::vector<float> input_data;
-    for (int i = 0; i < config.m_new_model_input_size; i++) {
-        input_data.push_back(i * 0.000001f);
+    // Fill an AudioBuffer with some data
+    anira::AudioBufferF input(1, m_inference_config.m_input_sizes[m_inference_config.m_index_audio_data[anira::Input]]);
+    for(int i = 0; i < m_inference_config.m_input_sizes[m_inference_config.m_index_audio_data[anira::Input]]; ++i) {
+        input.set_sample(0, i, i * 0.000001f);
     }
 
-    // Define the shape of input tensor
-    std::vector<int64_t> input_shape = config.m_model_input_shape_onnx;
+    std::vector<anira::MemoryBlock<float>> m_input_data;
+    std::vector<Ort::Value> m_inputs;
+    std::vector<Ort::Value> m_outputs;
 
-    // Create input tensor object from input data values and shape
-    const Ort::Value input_tensor = Ort::Value::CreateTensor<float>  (memory_info,
-                                                                    input_data.data(),
-                                                                    config.m_new_model_input_size,
-                                                                    input_shape.data(),
-                                                                    input_shape.size());
+    m_input_data.resize(m_inference_config.m_input_sizes.size());
+    m_inputs.clear();
+    for (size_t i = 0; i < m_inference_config.m_input_sizes.size(); i++) {
+        m_input_data[i].resize(m_inference_config.m_input_sizes[i]);
+        if (i != m_inference_config.m_index_audio_data[anira::Input]) {
+            m_input_data[i].clear();
+            m_inputs.emplace_back(Ort::Value::CreateTensor<float>(
+                m_memory_info,
+                m_input_data[i].data(),
+                m_input_data[i].size(),
+                m_inference_config.m_model_input_shape_onnx[i].data(),
+                m_inference_config.m_model_input_shape_onnx[i].size()
+            ));
+        } else {
+            m_inputs.emplace_back(Ort::Value::CreateTensor<float>(
+                m_memory_info,
+                input.data(),
+                input.get_num_samples(),
+                m_inference_config.m_model_input_shape_onnx[i].data(),
+                m_inference_config.m_model_input_shape_onnx[i].size()
+            ));
+        }
+    }
 
-    for (int i = 0; i < input_tensor.GetTensorTypeAndShapeInfo().GetShape().size(); ++i) {
-        std::cout << "Input shape " << i << ": " << input_tensor.GetTensorTypeAndShapeInfo().GetShape()[i] << std::endl;
+    for (int i = 0; i < m_inputs.size(); ++i) {
+        std::cout << "Input shape " << i << ": [" << m_inputs[i].GetTensorTypeAndShapeInfo().GetShape() << "]" << std::endl;
     }
 
     // Get input and output names from model
-    Ort::AllocatedStringPtr input_name = session.GetInputNameAllocated(0, ort_alloc);
-    Ort::AllocatedStringPtr output_name = session.GetOutputNameAllocated(0, ort_alloc);
-    const std::array<const char *, 1> input_names = {(char*) input_name.get()};
-    const std::array<const char *, 1> output_names = {(char*) output_name.get()};
-
-    // Define output tensor vector
-    std::vector<Ort::Value> output_tensors;
+    Ort::AllocatedStringPtr m_input_name = m_session.GetInputNameAllocated(0, m_ort_alloc);
+    Ort::AllocatedStringPtr m_output_name = m_session.GetOutputNameAllocated(0, m_ort_alloc);
+    const std::array<const char *, 1> m_input_names = {(char*) m_input_name.get()};
+    const std::array<const char *, 1> m_output_names = {(char*) m_output_name.get()};
 
     try {
-        // Run inference
-        output_tensors = session.Run(Ort::RunOptions{nullptr}, input_names.data(), &input_tensor, input_names.size(), output_names.data(), output_names.size());
-    }
-    catch (Ort::Exception &e) {
-        std::cout << e.what() << std::endl;
+        m_outputs = m_session.Run(Ort::RunOptions{nullptr}, m_input_names.data(), m_inputs.data(), m_input_names.size(), m_output_names.data(), m_output_names.size());
+    } catch (Ort::Exception &e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    for (int i = 0; i < output_tensors[0].GetTensorTypeAndShapeInfo().GetShape().size(); ++i) {
-        std::cout << "Output shape " << i << ": " << output_tensors[0].GetTensorTypeAndShapeInfo().GetShape()[i] << std::endl;
+    for (int i = 0; i < m_outputs.size(); ++i) {
+        std::cout << "Output shape " << i << ": [" << m_outputs[i].GetTensorTypeAndShapeInfo().GetShape() << "]" << std::endl;
     }
 
-    // Define output vector
-    const float* output_data = output_tensors[0].GetTensorData<float>();
+    std::vector<anira::MemoryBlock<float>> m_output_data;
+    m_output_data.resize(m_outputs.size());
 
-    // Extract the output tensor data
-    for (int i = 0; i < config.m_new_model_output_size; i++) {
-        std::cout << "Output data [" << i << "]: " << output_data[i] << std::endl;
+    for (size_t i = 0; i < m_outputs.size(); i++) {
+        const auto output_read_ptr = m_outputs[i].GetTensorMutableData<float>();
+        m_output_data[i].resize(m_inference_config.m_output_sizes[i]);
+
+        for (size_t j = 0; j < m_inference_config.m_output_sizes[i]; j++) {
+            std::cout << "Output data [" << i << "][" << j << "]: " << output_read_ptr[j] << std::endl;
+            m_output_data[i][j] = output_read_ptr[j];
+        }
+    }
+
+    // Copy the data to the output_data vector
+    for (int i = 0; i < m_output_data.size(); i++) {
+        for (int j = 0; j < m_output_data[i].size(); j++) {
+            //std::cout << "Output data [" << i << "][" << j << "]: " << m_output_data[i][j] << std::endl;
+        }
     }
 }
 
