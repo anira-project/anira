78796a5de696c4d21509d6a1938023b5ad5f68c4
Max Weidauer <max+git@weidauer.art>
2024-10-31 15:35:46 +0100

    updated function calls to snake case

--- test/test_audiobuffer.cpp
+++ test/test_audiobuffer.cpp
@@ -13,19 +13,19 @@ static double randomDouble(int LO, int HI){
 using namespace anira;
 TEST(Test_Audiobuffer, simple_write){
     AudioBufferF buffer = AudioBufferF(1,10);
-    for (size_t i = 0; i < buffer.getNumSamples(); i++){
-        EXPECT_FLOAT_EQ(0.f, buffer.getSample(0,i));
+    for (size_t i = 0; i < buffer.get_num_samples(); i++){
+        EXPECT_FLOAT_EQ(0.f, buffer.get_sample(0,i));
     }
 
-    buffer.setSample(0,5, 0.9f);
+    buffer.set_sample(0,5, 0.9f);
     
-    for (size_t i = 0; i < buffer.getNumSamples(); i++){
+    for (size_t i = 0; i < buffer.get_num_samples(); i++){
         float expected = i == 5 ? 0.9f : 0.f;
-        EXPECT_FLOAT_EQ(expected, buffer.getSample(0,i));
+        EXPECT_FLOAT_EQ(expected, buffer.get_sample(0,i));
     }
 
     buffer.clear();
-    for (size_t i = 0; i < buffer.getNumSamples(); i++){
-        EXPECT_FLOAT_EQ(0.f, buffer.getSample(0,i));
+    for (size_t i = 0; i < buffer.get_num_samples(); i++){
+        EXPECT_FLOAT_EQ(0.f, buffer.get_sample(0,i));
     }
 }
