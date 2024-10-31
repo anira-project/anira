47ea1918a92a8412d1fdc1f1b69be2453261c250
Fares Schulz <schulzfares@gmail.com>
2024-10-31 23:15:08 +0100

    Updated simple gain config shapes

--- extras/desktop/models/model-pool/SimpleGainConfig.h
+++ extras/desktop/models/model-pool/SimpleGainConfig.h
@@ -6,8 +6,8 @@
 static anira::InferenceConfig gain_config(
 #ifdef USE_LIBTORCH
     SIMPLEGAIN_MODEL_PATH + std::string("/simple_gain_network_mono.pt"),
-    {{512, 1, 1}, {1}}, // Input shapes: audio data and gain
-    {{512, 1, 1}, {1}}, // Output shapes: processed audio and peak gain
+    {{1, 1, 512}, {1}}, // Input shapes: audio data and gain
+    {{1, 1, 512}, {1}}, // Output shapes: processed audio and peak gain
 #endif
 #ifdef USE_ONNXRUNTIME
     SIMPLEGAIN_MODEL_PATH + std::string("/simple_gain_network_mono.onnx"),
@@ -16,8 +16,8 @@ static anira::InferenceConfig gain_config(
 #endif
 #ifdef USE_TFLITE
     SIMPLEGAIN_MODEL_PATH + std::string("/simple_gain_network_mono.tflite"),
-    {{1, 512, 1}, {1}}, // Input shapes: audio data and gain
-    {{1, 512, 1}, {1}}, // Output shapes: processed audio and peak gain
+    {{1, 1, 512}, {1}}, // Input shapes: audio data and gain
+    {{1, 1, 512}, {1}}, // Output shapes: processed audio and peak gain
 #endif
     5.f,
     0,
