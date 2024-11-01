41caf33903bd6f16d0b98cd770357279863aa6e0
Fares Schulz <schulzfares@gmail.com>
2024-11-01 22:55:41 +0100

    Updated Clap demo for new HostAudioConfig

--- examples/desktop/clap-audio-plugin/anira-clap-demo.cpp
+++ examples/desktop/clap-audio-plugin/anira-clap-demo.cpp
@@ -162,7 +162,7 @@ bool AniraClapPluginExample::init() noexcept {
 bool AniraClapPluginExample::activate(double sampleRate, uint32_t minFrameCount,
                              uint32_t maxFrameCount) noexcept
 {
-    anira::HostAudioConfig config (1, (size_t) maxFrameCount, sampleRate);
+    anira::HostAudioConfig config ((size_t) maxFrameCount, sampleRate);
 
     if (m_clap_thread_pool && m_clap_thread_pool->request_exec) {
         config.submit_task_to_host_thread = [this](int number_of_tasks) -> bool {
