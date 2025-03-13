1f6b459f528caddb358833a12a19124f3f49d4c9
Valentin Ackva <valentin.ackva@insone.de>
2025-03-13 15:34:01 +0100

    Preventing a race condition in the InferenceThread. The derived class's context was being destroyed before the base class destructor signalled the thread to stop, which means there is the chance for the thread to call deleted context while running

--- src/scheduler/InferenceThread.cpp
+++ src/scheduler/InferenceThread.cpp
@@ -7,6 +7,10 @@ InferenceThread::InferenceThread(moodycamel::ConcurrentQueue<InferenceData>& nex
 {
 }
 
+InferenceThread::~InferenceThread() {
+    stop();
+}
+
 void InferenceThread::run() {
     while (!should_exit()) {
         constexpr std::array<int, 2> iterations = {4, 32};
