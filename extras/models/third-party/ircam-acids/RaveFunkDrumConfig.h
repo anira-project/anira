40162f48e26d258dfde5e67ea4873fdd6c3757e3
Valentin Ackva <valentin.ackva@gmail.com>
2025-08-27 16:07:21 +0200

    Added missing preprocessor flag in RaveFunkDrumConfig.h

--- extras/models/third-party/ircam-acids/RaveFunkDrumConfig.h
+++ extras/models/third-party/ircam-acids/RaveFunkDrumConfig.h
@@ -10,7 +10,9 @@ static std::vector<anira::ModelData> model_data_rave_funk_drum_config = {
 };
 
 static std::vector<anira::TensorShape> tensor_shape_rave_funk_drum_config = {
+#ifdef USE_LIBTORCH
     {{{1, 1, 2048}}, {{1, 1, 2048}}, anira::InferenceBackend::LIBTORCH},
+#endif
 };
 
 static anira::ProcessingSpec processing_spec_rave_funk_drum_config{
