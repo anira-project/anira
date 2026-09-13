e2e84649e680005daf8cfa645cf3c73334af3760
Valentin Ackva <valentin.ackva@gmail.com>
2026-09-13 11:12:01 +0200

    cmake: executorch/litert/tflite default to their -2 re-cuts (Android -gpu bundles)

--- cmake/backends.cmake
+++ cmake/backends.cmake
@@ -89,6 +89,8 @@ function(_anira_engine_tag id version out)
         set(_recut "")
         if(id STREQUAL "onnxruntime")
             set(_recut "-2")   # -2: WebGPU EP + Dawn in the macOS/Windows -gpu archives as well
+        elseif(id STREQUAL "executorch" OR id STREQUAL "litert" OR id STREQUAL "tflite")
+            set(_recut "-2")   # -2: Android -gpu bundles (Vulkan / LiteRT accelerators / OpenCL delegate)
         endif()
         set(${out} "${id}-v${version}${_recut}" PARENT_SCOPE)
     endif()
